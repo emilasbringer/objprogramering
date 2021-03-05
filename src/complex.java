b89490ed7d988d7b2a08086883c204e944ab8119
emilasbringer <emil.asbringer@elev.ga.ntig.se>
2021-03-05 11:11:28 +0100

    multiply funktion, argument av komplext tal + kommentarer

--- src/complex.java
+++ src/complex.java
@@ -1,32 +1,36 @@
- public class complex {
-        public static void main (String[] arg) {
+
+public class complex {
+    public static void main(String[] arg) {
 
 
-            complexObj a = new complexObj(3.0,2.0);
-            complexObj b = new complexObj(4.0,1.0);
+        complexObj a = new complexObj(3.0, 2.0);
+        complexObj b = new complexObj(4.0, 1.0);
 
-            System.out.println(a.real);
-            System.out.println(b.imaginary);
+        System.out.println(a.real);
+        System.out.println(b.imaginary);
 
+        complexObj c = a.add(b);
+        complexObj d = new complexObj(7, 3);
 
-
-            complexObj c = a.add(b);
-            complexObj d = new complexObj(7,3);
-
-            System.out.println(c);
-            if (c.equals(d)) {
-                System.out.println("add funkar!!");
-            }
-/*
-            c = a.multiply(b);
-            d = new complexnr(10,11);
-
-            System.out.println(c);
-            if (c.equals(d)) {
-                System.out.println("multiply funkar!!");
-            }
-*/
-
-
+        System.out.println(c.real + " " + c.imaginary);
+        if (c.equals(d)) {
+            System.out.println("add funkar!!");
         }
+        System.out.println(a.real + " " + a.imaginary);
+
+        c = a.multiply(b);
+        d = new complexObj(10, 11);
+
+        System.out.println(c.real + " " + c.imaginary);
+        if (c.equals(d)) {
+            System.out.println("multiply funkar!!");
+        }
+
+        System.out.println("Argumentet till 3+2i är " + a.arg());
+        if (a.arg() == Math.atan(2.0 / 3)) {
+            System.out.println("arg() funkar!!");
+        }
+
     }
+}
+
--- src/complexObj.java
+++ src/complexObj.java
@@ -1,16 +1,58 @@
+import java.util.Objects;
+
 public class complexObj {
 
     double real;
     double imaginary;
 
+//konstruktor
+
     public complexObj (double real, double imaginary){
         this.real = real;
         this.imaginary = imaginary;
     }
+//add funktion
 
-    public complexObj add(complexObj a) {
-        this.real + a.real
-        this.imaginary + a.real
+    public complexObj add(complexObj b) {
 
+       double zreal = this.real + b.real;
+       double zimaginary = this.imaginary + b.imaginary;
+       return new complexObj(zreal, zimaginary);
+    }
+
+//multipliceringsfunktion
+
+    public complexObj multiply(complexObj a) {
+
+        double zreal = (this. real * a.real) - (this.imaginary * a.imaginary);
+
+        double zimaginary = (this.real * a.imaginary) + (this.imaginary * a.real);
+
+        return new complexObj(zreal, zimaginary);
+    }
+
+//argumentet för det komplexa talet
+
+    public double arg() {
+
+        double angle = Math.atan(this.imaginary / this.real);
+
+        return angle;
+    }
+
+//override för komplextal equals komplextal
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        complexObj that = (complexObj) o;
+        return Double.compare(that.real, real) == 0 &&
+                Double.compare(that.imaginary, imaginary) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(real, imaginary);
     }
 }
